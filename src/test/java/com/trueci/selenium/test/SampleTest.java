426731408c020de8eb2ecdb75a2ef6d57b66fcd6
aloveria <32290851+aloveria@users.noreply.github.com>
2018-03-06 12:17:34 +0800

    Update SampleTest.java

--- src/test/java/com/trueci/selenium/test/SampleTest.java
+++ src/test/java/com/trueci/selenium/test/SampleTest.java
@@ -11,7 +11,7 @@ public class SampleTest extends BaseClass{
 	public void getPageTitle(){
 		
 		String title = getDriver().getTitle();
-		Assert.assertEquals(title, "Training Portal");
+		Assert.assertEquals(title, "Hello");
 		disposeDriver();
 	}
 	
